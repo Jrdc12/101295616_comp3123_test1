cb3619ad5e0eba74b1b899968818af36363f5ac7
Jrdc12 <57374686+Jrdc12@users.noreply.github.com>
2022-10-05 23:04:48 -0400

    Changed question 3 to output log files in console

--- question-3/question3.js
+++ question-3/question3.js
@@ -12,6 +12,7 @@ const makeLogs = () => {
     process.chdir(target);
     for (let i = 0; i < 10; i++) {
         fs.writeFileSync(`log${i}.txt`, `This is log file ${i}`);
+        console.log(`log${i}.txt`, `This is log file ${i}`)
     }
 }
 
@@ -19,7 +20,13 @@ const deleteLogs = () => {
     const target = './logs';
     if (fs.existsSync(target)) {
         fs.readdirSync(target).forEach((file) => {
-            fs.unlinkSync(`${target}/${file}`);
+            for (let i = 0; i < 10; i++) {
+                if (file === `log${i}.txt`) {
+                    fs.unlinkSync(`${target}/${file}`);
+                    console.log(`delete files ${file}`)
+                }
+            }
+            //fs.unlinkSync(`${target}/${file}`);
         });
         fs.rmdirSync(target);
     }
